1126826a427866aa13a2457cb5e516cb6fa2edc8
itsmanu4u <mohan.2kit@gmail.com>
2018-08-23 16:05:02 +0530

    Version(2) with Data Export options and minor bug fix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+![Output of DjangoWeb Project!](https://github.com/itsmanu4u/DjangoWeb/blob/master/XtractIT.png)
+
 ===================================
-	# XtractIT (V.1)
+	# XtractIT (V.2)
 
 # You can Edit/Modify this simple Python script, even without asking. However, it’s always a good gesture to give credit where it’s due.
 
@@ -14,6 +16,10 @@ Python program to extract Email and contact information from websites, Blogs &am
 
 2. Enter the Website, Blog, Discussion-Forum or any E-Site that contains E-mails & contact numbers.
 http://www.example.com
+
+3. Now, with XtractIT V3, you can Export extracted data into CSV(Excel).
+
+4. Still under development process.
 
 THAT'S IT !!.  You have your data to SPAM them(LOL). Please use it for constructive purpose, don't bother someone who dosn't bother you, as that simple.
 
--- XtractIT.py
+++ XtractIT.py
@@ -4,7 +4,21 @@ email addresses and US phone numbers
 Author: Manmohan Chauhan
 https://github.com/itsmanu4u
 '''
+print('\n\n======================| XtractIT V.2 |========================')
 
+print("""
+
+
+		      ("`-''-/").___..--''"`-._
+                       `6_ 6  )   `-.  (     ).`-.__.`)
+                       (_Y_.)'  ._   )  `._ `. ``-..-'
+                     _..`--'_..-_/  /--'_.' ,'
+                    (il),-''  (li),'  ((!.-'
+""")
+print('===============================================================')
+
+
+import time
 from bs4 import BeautifulSoup
 import re
 from urllib.request import urlopen
@@ -21,12 +35,12 @@ s = s.get_text()
 
 phone = re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})",s)
 emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",s)
-print('\n\n=================| MyWebScrappy |=================')   
+
+print('\n\n=================| XtractIT |=================')   
 if len(phone) == 0:
     print ("Sorry, no phone number found.")
-
-    print('----------------------------------------')
     print ()
+
 else :
     count = 1
     for item in phone:
@@ -38,7 +52,6 @@ print()
 
 if len(emails) == 0:
     print("Sorry, no email address found.")
-    print('\n\n==================================================\n')
     print()
 else:
     count = 1
@@ -46,4 +59,21 @@ else:
         print(count, ' email address(es) found : ', item)
         count += 1
 
-print('')
+print('==================================================')
+print('\n \n')
+
+pref = input('Do you want to Export data into Excel ? (Y/N) : ')
+
+if pref == 'Y':
+    print('\n Please wait, exporting data.....')
+    time.sleep(4)
+
+    lst = (phone, emails)
+    saveFile = open('Xtract.csv','w')
+    saveFile.write(str(phone +  emails))
+    saveFile.close()
+else:
+    print('\n Closing Program...')
+    time.sleep(2)
+
+
